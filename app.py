9e976fa850ca1757cb58289798cdbea51db185f1
P4tt4te <57622377+P4tt4te@users.noreply.github.com>
2024-02-09 13:55:09 +0100

    fix template and app.py

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import joblib
+import numpy as np
 
 app = Flask(__name__)
 
+model = joblib.load('./artifacts/regmodel.pkl')
+
 @app.route("/")
-def hello():
-    return render_template('index.html')
+def index():
+    return render_template('home.html',prediction_text="")
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    features = [float(x) for x in request.form.values()]
+    input_features = [np.array(features)]
+    prediction = model.predict(input_features)
+
+    return render_template('home.html', prediction_text=f"Predicted House Price: ${prediction[0]:.2f}")
 
 if __name__=="__main__":
     app.run(debug=True)
